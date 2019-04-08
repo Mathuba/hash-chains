b66ece164acd4f06a7bd34c662bfbd93d40ee213
Mathuba <mathuba58@gmail.com>
2019-04-08 13:37:43 +0200

    Added method to add string along with test cases

--- hash_chains.py
+++ hash_chains.py
@@ -10,9 +10,11 @@ class ListNode:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.size = 0
 
     def prepend(self, value):
         self.head = ListNode(data=value, next=self.head)
+        self.size += 1
 
     def find(self, value):
         current = self.head
@@ -85,6 +87,16 @@ class QueryProcessor:
     def write_chain(self, chain):
         print(' '.join(chain))
 
+    def add_to_hash_table(self, q_string):
+        hash_value = self._hash_func(q_string)
+        if not self.elems[hash_value]:
+            add_list = LinkedList()
+            add_list.prepend(q_string)
+            self.elems[hash_value] = add_list
+        else:
+            add_list = self.elems[hash_value]
+            add_list.prepend(q_string)
+
     def read_query(self, test_num=None):
         if test_num is None:
             return Query(input().split())
@@ -101,33 +113,38 @@ class QueryProcessor:
             return Query('add MathubaMzwandile'.split())
         elif test_num == 6:
             return Query('add'.split())
+        elif test_num == 7:
+            return Query('add JGxqTHXXed'.split())
+        elif test_num == 8:
+            return Query('add FErhDFUKGkOAWPE'.split())
         else:
             return Query('illegal commans'.split())
 
     def process_query(self, query):
-        if query.type == "check":
-            # use reverse order, because we append strings to the end
-            self.write_chain(cur for cur in reversed(self.elems)
+        if query.type is not None:
+            if query.type == "check":
+                # use reverse order, because we append strings to the end
+                self.write_chain(cur for cur in reversed(self.elems)
                         if self._hash_func(cur) == query.ind)
-        else:
-            try:
-                ind = self.elems.index(query.s)
-            except ValueError:
-                ind = -1
-            if query.type == 'find':
-                self.write_search_result(ind != -1)
-            elif query.type == 'add':
-                if ind == -1:
-                    self.elems.append(query.s)
             else:
-                if ind != -1:
-                    self.elems.pop(ind)
+                try:
+                    ind = self.elems.index(query.s)
+                except ValueError:
+                    ind = -1
+                if query.s is not None:
+                    if query.type == 'find':
+                        self.write_search_result(ind != -1)
+                    elif query.type == 'add':
+                        self.add_to_hash_table(query.s)
+                    else:
+                        if ind != -1:
+                            self.elems.pop(ind)
 
     def process_queries(self):
         n = int(input())
         for i in range(n):
-            self.read_query()  # should be deleted eventually
-            #self.process_query(self.read_query())
+            # self.read_query()  # should be deleted eventually
+            self.process_query(self.read_query())
 
 
 if __name__ == '__main__':
--- tests/test_queryProcessor.py
+++ tests/test_queryProcessor.py
@@ -1,4 +1,4 @@
-from hash_chains import QueryProcessor, Query
+from hash_chains import QueryProcessor, Query, LinkedList
 import pytest
 
 
@@ -59,6 +59,7 @@ def test_correct_word_length_accepted(query_processor):
 def test_correct_hash_value_returned(query_processor):
     assert query_processor._hash_func('Mathuba') == 2
 
+
 def test_correct_hash_value_two():
     proc_two = QueryProcessor(41)
     assert proc_two._hash_func('NLZLNyXoXoIC') == 15
@@ -67,3 +68,55 @@ def test_correct_hash_value_two():
 def test_special_characters_return_no_hash_value(query_processor):
     assert query_processor._hash_func('Mathu(ba') is None
 
+
+def test_add_creates_new_list_in_empty_slot(query_processor):
+    add_cmd = query_processor.read_query(1)
+    query_processor.process_query(add_cmd)
+    add_list = query_processor.elems[2]
+    add_node = add_list.head
+    assert isinstance(add_list, LinkedList)
+    assert add_node.data == 'MathubaMzwandil'
+    assert add_node.next is None
+
+
+def test_add_new_size_is_one(query_processor):
+    add_cmd = query_processor.read_query(1)
+    query_processor.process_query(add_cmd)
+    add_list = query_processor.elems[2]
+    add_node = add_list.head
+    assert add_list.size == 1
+
+
+def test_add_not_created_for_invalid_length(query_processor):
+    add_cmd = query_processor.read_query(5)
+    query_processor.process_query(add_cmd)
+    add_list = query_processor.elems[3]
+    assert add_list is None
+
+
+def test_list_prepended_in_existing_slot(query_processor):
+    add_cmd = query_processor.read_query(1)
+    ant_cmd = query_processor.read_query(7)
+    query_processor.process_query(add_cmd)
+    query_processor.process_query(ant_cmd)
+    add_list = query_processor.elems[2]
+    add_node = add_list.head
+    assert add_list.size == 2
+    assert add_node.next.data == 'MathubaMzwandil'
+    assert add_node.data == 'JGxqTHXXed'
+
+
+def test_add_ignores_empty_string(query_processor):
+    add_cmd = query_processor.read_query(6)
+    query_processor.process_query(add_cmd)
+    assert query_processor.elems == [None, None, None, None, None]
+
+
+def test_add_to_slot_zero(query_processor):
+    add_cmd = query_processor.read_query(8)
+    query_processor.process_query(add_cmd)
+    add_list = query_processor.elems[0]
+    add_node = add_list.head
+    assert isinstance(add_list, LinkedList)
+    assert add_node.data == 'FErhDFUKGkOAWPE'
+    assert add_node.next is None
